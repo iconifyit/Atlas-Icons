5a7762665ce63aa1f3bdf8447b4c1ccf230d9056
Scott Lewis <scott@atomiclotus.net>
2022-11-10 20:33:06 -0500

    chore: delete moved folders

--- client/src/shared/baseUrl.js
+++
@@ -1,2 +0,0 @@
-export const BASE_URL = "http://localhost:5000/api/";
-export const ICONS_PATH = "http://localhost:5000/uploads/";
--- server/db/conn.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const db = process.env.DATABASE_URL;
-
-module.exports = function() {
-  mongoose
-    .connect(db, { useNewUrlParser: true,  useUnifiedTopology: true  })
-    .then(() => console.log("MongoDB successfully connected"))
-    .catch(err => console.log(err));
-};
